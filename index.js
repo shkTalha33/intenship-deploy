22183ee7cb8a233bc7dae7ab175fe77d1f73cf90
Muhammad Talha <128708311+shkTalha33@users.noreply.github.com>
2024-10-13 16:17:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-const express = require("express")
-const cors = require("cors")
-const connectToMongo = require("./db")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const connectToMongo = require("./db");
+const app = express();
 
+// CORS configuration
+const corsOptions = {
+  origin: "https://khazany-shoes33.vercel.app", // Frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  optionsSuccessStatus: 200, // Legacy browsers choke on 204, so set it to 200
+};
 
- app.use(express.json())
- app.use(cors())
-require("dotenv").config()
-connectToMongo()
+app.use(express.json());
+app.use(cors(corsOptions)); // Enable CORS with the specified options
 
-// routes 
-app.use("/items",require("./Routes/ListItemRoutes"))
-app.use("/auth",require("./Routes/AuthRoutes"))
-app.use("/products",require("./Routes/ProductRoutes"))
+require("dotenv").config();
+connectToMongo();
 
-const port = process.env.PORT
+// Routes
+app.use("/items", require("./Routes/ListItemRoutes"));
+app.use("/auth", require("./Routes/AuthRoutes"));
+app.use("/products", require("./Routes/ProductRoutes"));
 
-app.listen(port,()=>{
-    console.log(`Server is running on ${port} port`)
-})
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
